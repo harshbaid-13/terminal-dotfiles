2c65a7bfa7d5fef68727034a7f018d4777aa1e42
harshbaid-13 <harshbaid.ind@gmail.com>
2026-09-14 11:27:54 +0530

    backup before windows install

--- config/wezterm/keys.lua
+++ config/wezterm/keys.lua
@@ -4,13 +4,36 @@ local act = wezterm.action
 local M = {}
 
 function M.setup(config)
-	config.keys = {
-		-- handle vim's TComment
-		{ key = "/", mods = "CTRL", action = wezterm.action({ SendString = "\x1f" }) },
-		{ key = "RightArrow", mods = "CTRL", action = act.SwitchWorkspaceRelative(1) },
-		{ key = "LeftArrow", mods = "CTRL", action = act.SwitchWorkspaceRelative(-1) },
-		{ key = "x", mods = "CTRL", action = act.CloseCurrentTab({ confirm = false }) },
-	}
+    config.keys = {
+        -- handle vim's TComment
+        {
+            key = "/",
+            mods = "CTRL",
+            action = wezterm.action({ SendString = "\x1f" }),
+        },
+
+        -- Switch workspace
+        {
+            key = "RightArrow",
+            mods = "CTRL",
+            action = act.SwitchWorkspaceRelative(1),
+        },
+
+        {
+            key = "LeftArrow",
+            mods = "CTRL",
+            action = act.SwitchWorkspaceRelative(-1),
+        },
+
+        -- Close current tab
+        {
+            key = "w",
+            mods = "CTRL",
+            action = wezterm.action({
+                CloseCurrentTab = { confirm = false },
+            }),
+        },
+    }
 end
 
 return M
--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -11,11 +11,12 @@ local config = {}
 config.enable_wayland = true
 config.front_end = "WebGpu"
 config.color_scheme = "nord"
+
 config.use_fancy_tab_bar = false
 config.font_size = 13
 config.font = wezterm.font("JetBrainsMono Nerd Font", { weight = "DemiBold" })
-config.default_workspace = "scratch"
-config.window_background_opacity = 1
+config.default_workspace = "harsh"
+config.window_background_opacity = 0.8
 -- config.hide_tab_bar_if_only_one_tab = true
 config.max_fps = 120
 
